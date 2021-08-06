18bc933e6f0180ead52fbf5d8abe1f46dc641db2
ivy997 <49655087+ivy997@users.noreply.github.com>
2021-08-06 09:57:32 +0300

    Fix user edit function
    - Remove null properties from responses
    - Make list categories public
    - Fix categories pagination

--- Boutique/src/main/java/boutique/models/MessageResponse.java
+++ Boutique/src/main/java/boutique/models/MessageResponse.java
@@ -1,7 +1,10 @@
 package boutique.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Date;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MessageResponse {
     private String stackTrace;
     private String message;
--- Boutique/src/main/java/boutique/repositories/CategoryRepository.java
+++ Boutique/src/main/java/boutique/repositories/CategoryRepository.java
@@ -1,6 +1,8 @@
 package boutique.repositories;
 
 import boutique.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
@@ -10,4 +12,6 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Boolean existsByName(String name);
 
     Category findByName(String name);
+
+    Page<Category> findAll(Pageable paging);
 }
